fdd0c04e5c538d1e00f9b710de229c8b64f9f1c0
TienNob <nhuttien8377@gmail.com>
2022-05-12 23:30:59 +0700

    fix code

--- cart/cart.js
+++ cart/cart.js
@@ -54,6 +54,7 @@ var listItems = [
     pathImg: "../product/img/product99.png",
   },
 ];
+
 const cartTableBody = document.querySelector("tbody");
 let items = [];
 let oderNums = [];
@@ -65,60 +66,63 @@ for (let i = 0; i < localStorage.length; i++) {
   oderNums.push(parseInt(localStorage.getItem(keyProduct)));
   itemSums.push(listItems[keyProduct].price * oderNums[i]);
 }
-console.log(items.length);
-const itemsHtml = items.map((item, i) => {
-  return `<tr class="cart-table_row">          
-    <td class="pt-3 pb-3 text-start">
-      <img width="100px" src="${item.pathImg}" alt="">
-      <b class="mt-2 d-block d-sm-inline-block text-sm-start text-center">${item.name}</b>
-    </td>
-    <td><small>${item.price}đ</small></td>
-    <td><small>${oderNums[i]}</small></td>
-    <td><small>${itemSums[i]}đ</small></td>
-    <td class="text-end"><i class="fa-solid fa-xmark cart-remove"></i></td>          
-  </tr>`;
-});
-const emptyTable = [
-  `<tr>
-    <td class="text-center pt-4" colspan="5">Bạn chưa có sản phẩm nào, vui lòng thêm sản phẩm ở mục sản phẩm!!</td>
-  </tr>`,
-];
-items.length !== 0
-  ? (cartTableBody.innerHTML = itemsHtml.join(""))
-  : (cartTableBody.innerHTML = emptyTable.join());
-const cartRemoves = document.querySelectorAll(".cart-remove");
-cartRemoves.forEach((remove, i) => {
-  remove.addEventListener("click", () => {
-    const keyProduct = localStorage.key(i);
-    if (keyProduct) {
-      localStorage.removeItem(keyProduct);
-      location.reload();
-    }
+// show item cart
+function showItem() {
+  const itemsHtml = items.map((item, i) => {
+    return `<tr class="cart-table_row">          
+      <td class="pt-3 pb-3 text-start">
+        <img width="100px" src="${item.pathImg}" alt="">
+        <b class="mt-2 d-block d-sm-inline-block text-sm-start text-center">${item.name}</b>
+      </td>
+      <td><small>${item.price}đ</small></td>
+      <td><small>${oderNums[i]}</small></td>
+      <td><small>${itemSums[i]}đ</small></td>
+      <td class="text-end"><i class="fa-solid fa-xmark cart-remove"></i></td>          
+    </tr>`;
   });
-});
-
+  const emptyTable = [
+    `<tr>
+      <td class="text-center pt-4" colspan="5">Bạn chưa có sản phẩm nào, vui lòng thêm sản phẩm ở mục sản phẩm!!</td>
+    </tr>`,
+  ];
+  items.length !== 0
+    ? (cartTableBody.innerHTML = itemsHtml.join(""))
+    : (cartTableBody.innerHTML = emptyTable.join());
+  const cartRemoves = document.querySelectorAll(".cart-remove");
+  cartRemoves.forEach((remove, i) => {
+    remove.addEventListener("click", () => {
+      const keyProduct = localStorage.key(i);
+      if (keyProduct) {
+        localStorage.removeItem(keyProduct);
+        location.reload();
+      }
+    });
+  });
+}
 // calc
-const cartSum = document.querySelector(".cart-total_sum");
-console.log(itemSums);
-const sumItem = itemSums.reduce((acc, item) => {
-  return acc + item;
-}, 0);
-cartSum.innerText = sumItem + "đ";
-const totalEnd = document.querySelector(".cart-total_end");
-const btnPay = document.querySelector(".btn-pay");
-const totalShip = document.querySelector(".cart-total_ship");
-if (items.length !== 0) {
-  totalEnd.innerText = sumItem + 20000 + "đ";
-  totalShip.innerText = 20000 + "đ";
-  btnPay.addEventListener("click", () => {
-    alert("Cảm ơn bạn đã thanh toán");
-  });
-} else {
-  totalEnd.innerText = 0 + "đ";
-  totalShip.innerText = 0 + "đ";
-  btnPay.addEventListener("click", () => {
-    alert("Vui lòng thêm sản phẩm để thanh toán");
-  });
+function totalCost() {
+  const cartSum = document.querySelector(".cart-total_sum");
+  console.log(itemSums);
+  const sumItem = itemSums.reduce((acc, item) => {
+    return acc + item;
+  }, 0);
+  cartSum.innerText = sumItem + "đ";
+  const totalEnd = document.querySelector(".cart-total_end");
+  const btnPay = document.querySelector(".btn-pay");
+  const totalShip = document.querySelector(".cart-total_ship");
+  if (items.length !== 0) {
+    totalEnd.innerText = sumItem + 20000 + "đ";
+    totalShip.innerText = 20000 + "đ";
+    btnPay.addEventListener("click", () => {
+      alert("Cảm ơn bạn đã thanh toán");
+    });
+  } else {
+    totalEnd.innerText = 0 + "đ";
+    totalShip.innerText = 0 + "đ";
+    btnPay.addEventListener("click", () => {
+      alert("Vui lòng thêm sản phẩm để thanh toán");
+    });
+  }
 }
 // footer email
 const footerEmail = document.querySelector(".footer-mail_send_input");
@@ -130,3 +134,6 @@ footerBtn.addEventListener("click", () => {
     );
   }
 });
+
+showItem();
+totalCost();
